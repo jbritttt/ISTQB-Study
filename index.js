a01b6275ec10bbcc5826dbb9795312e80fca88ec
Jonathan Britton <jbritttt29@hotmail.com>
2024-02-16 22:13:55 +0000

    functionality to hide and show all answers

--- index.js
+++ index.js
@@ -4,33 +4,22 @@ let showAnswerBtn = document.querySelectorAll(".show-answer-btn");
 
 let userInput = document.querySelectorAll(".user-input");
 
+let btnAll = document.querySelector("#btn-all");
 
-
-userInput.forEach((item) => {
-
-    item.addEventListener("click", (e) => {
-       
-    
-       
-        
-      });
-  
+answers.forEach((item) => {
+  btnAll.addEventListener("click", (e) => {
+    item.classList.toggle("show");
+  });
 });
 
 showAnswerBtn.forEach((item) => {
   item.addEventListener("click", (e) => {
     item.nextElementSibling.classList.toggle("show");
 
-    if(item.textContent == 'Show'){
-        item.textContent = 'Hide'
-
-    }else{
-        item.textContent = 'Show'
-
+    if (item.textContent == "Show") {
+      item.textContent = "Hide";
+    } else {
+      item.textContent = "Show";
     }
-    
   });
 });
-
-
-
